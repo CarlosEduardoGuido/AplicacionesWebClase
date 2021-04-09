09c1a17b294841444e489080e8e6dc4e5b30a50d
Carlos Guido <al221910020@gmail.com>
2021-04-09 13:35:38 -0500

    Clase 25 de febrero

--- app/Http/Controllers/SistemController.php
+++ app/Http/Controllers/SistemController.php
@@ -5,10 +5,17 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Auth;
 use App\UsuariosModel;
+use App\EstadosModel;
+use App\MunicipiosModel;
 
 class SistemController extends Controller
 {
     public function entrada(){
             return view('entrada');
+    }
+    public function combos(){
+            $estados = EstadosModel::all();
+            return view('combos')
+                ->with(['estados'=>$estados]);
     }
 }
---
+++ app/MunicipiosModel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class MunicipiosModel extends Model{
+        protected $table = 'tb_municipios';
+        protected $primaryKey = 'id_municipio';
+        protected $fillable = ['nombre', 'id_estado'];
+}
